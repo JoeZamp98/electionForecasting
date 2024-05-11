d028679bc049c9d5bb46e5b06c41af43df3d3dff
JoeZamp98 <joezamp98@gmail.com>
2024-05-11 15:21:08 -0400

    Imported and scoped cleaning code for population dataset.  Added some additional calculation columns such as victory and margin per election in the vote history dataset.

--- data_analysis/presidentialDataAnalysis_2000_2020.py
+++ data_analysis/presidentialDataAnalysis_2000_2020.py
@@ -40,17 +40,70 @@ presPercent_pivoted = presPercent_pivoted[(pd.isna(presPercent_pivoted['2012']) 
 
 presPercent_pivoted.reset_index(inplace=True)
 
-presPercent_pivoted['Swing_16_20'] = presPercent_pivoted['2020'] - presPercent_pivoted['2016']
+presPercent_party_pivoted_20 = presPercent_pivoted.pivot_table(index=['state', 'county_name', 'mode'], columns=['party'], values='2020', aggfunc='mean')
 
-presPercent_pivoted['Swing_12_16'] = presPercent_pivoted['2016'] - presPercent_pivoted['2012']
+for column in presPercent_party_pivoted_20.columns:
+
+    presPercent_party_pivoted_20.rename(columns={column: column + '_20'}, inplace=True)
+
+presPercent_party_pivoted_16 = presPercent_pivoted.pivot_table(index=['state', 'county_name', 'mode'], columns=['party'], values='2016', aggfunc='mean')
+
+for column in presPercent_party_pivoted_16.columns:
+
+    presPercent_party_pivoted_16.rename(columns={column: column + '_16'}, inplace=True)
 
 
-presPercent_party_pivoted = presPercent_pivoted.pivot_table(index=['state', 'county_name', 'mode', '2012', '2016'], columns=['party'], values='2020', aggfunc='mean')
+presPercent_party_pivoted_12 = presPercent_pivoted.pivot_table(index=['state', 'county_name', 'mode'], columns=['party'], values='2012', aggfunc='mean')
 
-presPercent_party_pivoted.reset_index(inplace=True)
+for column in presPercent_party_pivoted_12.columns:
 
-# presPercent_party_pivoted = presPercent_party_pivoted.pivot_table(index=['state', 'county_name', 'mode', '2012'], values='2016', aggfunc='mean')
+    presPercent_party_pivoted_12.rename(columns={column: column + '_12'}, inplace=True)
 
-# presPercent_party_pivoted.reset_index(inplace=True)
+pres_mergedFullPivot = pd.merge(presPercent_party_pivoted_20, presPercent_party_pivoted_16, how='inner', left_index=True, right_index=True)
 
-# presPercent_party_pivoted = presPercent_party_pivoted.pivot_table(index=['state', 'county_name', 'mode'], values='2012', aggfunc='mean')
+pres_mergedFullPivot = pd.merge(pres_mergedFullPivot, presPercent_party_pivoted_12, how='inner', left_index=True, right_index=True)
+
+pres_mergedFullPivot.reset_index(inplace=True)
+
+def determine_victory(row, year):
+
+    if row['REPUBLICAN_' + year] > row['DEMOCRAT_' + year]:
+
+        return "Republican"
+    
+    elif row['REPUBLICAN_' + year] < row['DEMOCRAT_' + year]:
+
+        return "Democrat"
+    
+    else:
+
+        return "Other"
+    
+relevant_years = ['12', '16', '20']
+
+for year in relevant_years:
+
+    pres_mergedFullPivot['Victor_' + year] = pres_mergedFullPivot.apply(determine_victory,  args=(year,), axis=1)
+
+    pres_mergedFullPivot['Margin_' + year] = (pres_mergedFullPivot['REPUBLICAN_' + year] - pres_mergedFullPivot['DEMOCRAT_' + year])
+
+pres_mergedFullPivot['Swing_12_16'] = pres_mergedFullPivot['Margin_16'] - pres_mergedFullPivot['Margin_12']
+
+
+pres_mergedFullPivot['Swing_16_20'] = pres_mergedFullPivot['Margin_20'] - pres_mergedFullPivot['Margin_16']
+
+
+## -- Read In Population Data -- ##
+
+population_data_raw = pd.read_excel('../sourceData/populationData_2020_2022.xlsx')
+
+population_data_raw.columns = ['Location', 'April2020', 'July2020', 'July2021', 'July2022']
+
+population_data_raw = population_data_raw[4:-5]
+
+population_data_raw['Location'] = population_data_raw['Location'].replace(['\.', ' County', ' Parish'], '', regex=True).str.strip().str.upper()
+
+population_data_raw[['County', 'State']] = population_data_raw['Location'].str.split(',', expand=True)
+
+population_data_raw['percentChange'] = (population_data_raw['July2022'] / population_data_raw['July2020']) - 1
+
